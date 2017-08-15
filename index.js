f7ebc7d208ab276c3ccb10397a44ee57d9478781
Miles Perkins <mperkins1993@gmail.com>
2017-08-15 11:38:11 -0400

    auth flow

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session'); // to make cookies
+const passport = require('passport'); // to make passport know to make use of the cookies
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport'); //ES6 doesn't require you to add the .js at the end of a js file
@@ -7,6 +9,16 @@ require('./services/passport'); //ES6 doesn't require you to add the .js at the 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, //the age must be represented in milliseconds (this is 30 days in ms)
+    keys: [keys.cookieKey] //encrypts the key
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
--- services/passport.js
+++ services/passport.js
@@ -1,9 +1,22 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
 const User = mongoose.model('users');
+
+// function to take an existing user and generate a cookie
+passport.serializeUser((user, done) => {
+  //this user.id is the mongo id
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -23,6 +36,27 @@ passport.use(
           new User({ googleId: profile.id }) //creates model instance
             .save() //saves it to the db
             .then(user => done(null, user)); //creates a newer model instance
+        }
+      });
+    }
+  )
+);
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: keys.facebookClientID,
+      clientSecret: keys.facebookClientSecret,
+      callbackURL: '/auth/facebook/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ facebookId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({ facebookId: profile.id })
+            .save()
+            .then(user => done(null, user));
         }
       });
     }
