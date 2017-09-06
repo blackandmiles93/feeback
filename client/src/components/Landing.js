ff8609bbed36fc7620ccaacb93a818ffe540c0c6
Miles Perkins <mperkins1993@gmail.com>
2017-09-06 16:12:19 -0400

    minor fixes with routes

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Background from '../img/people.jpg';
 
 const Landing = () => {
   return (
-    <div style={{ textAlign: 'center' }}>
+    <div
+      style={{
+        textAlign: 'center'
+        // backgroundImage: `url(${Background})`,
+        // backgroundPosition: 'cover'
+      }}
+    >
       <h1>Feeback</h1>
       Collect feedback from your users to take your app to the next level
     </div>
