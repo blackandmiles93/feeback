f28b6fcc536745c8649687b7e0e4679c28b637af
Miles Perkins <mperkins1993@gmail.com>
2017-09-06 15:21:33 -0400

    adding production sendgrid

--- config/prod.js
+++ config/prod.js
@@ -9,5 +9,6 @@ module.exports = {
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   stripeSecretKey: process.env.STRIPE_SECRET_KEY,
   sendGridKey: process.env.SEND_GRID_KEY,
+  sendGridProdKey: process.env.SEND_GRID_PROD_KEY,
   redirectDomain: process.env.REDIRECT_DOMAIN
 };
