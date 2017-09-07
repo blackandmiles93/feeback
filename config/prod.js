20df92f4bfdbf724e6c6d5153370182eb38756df
Miles Perkins <mperkins1993@gmail.com>
2017-09-06 23:00:34 -0400

    hopefully final sendgrid fix

--- config/prod.js
+++ config/prod.js
@@ -9,6 +9,5 @@ module.exports = {
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   stripeSecretKey: process.env.STRIPE_SECRET_KEY,
   sendGridKey: process.env.SEND_GRID_KEY,
-  sendGridProdKey: process.env.SEND_GRID_PROD_KEY,
   redirectDomain: process.env.REDIRECT_DOMAIN
 };
